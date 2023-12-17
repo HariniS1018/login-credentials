dd73a05dac48def04ffbe2901f2d0a14753ac11c
Harini S <131979231+HariniS1018@users.noreply.github.com>
2023-12-17 14:31:02 +0530

    Update LoginMod.java

--- model/LoginMod.java
+++ model/LoginMod.java
@@ -3,18 +3,20 @@ package com.loginpackage;
 public class LoginMod extends SuperFuncs{
 	public static boolean retrieveUser(String name, String pwd){
 		String dbuser, dbpass = null;
+		boolean result = false;
 		try {
 			Statement stmt = con.createStatement();
 			rs = stmt.executeQuery("select username,password from security where username = \" " + user + "\"");
 			dbuser = rs.getString(1);
 			dbpass = rs.getString(2);
 			if(user.equals(dbuser)&& pwd.equals(dbpass))
-			        return true;
+			        result = true;
 			else
-			        return false;
+			        result = false;
 		}
     		catch(Exception e) {
 			System.out.println(e);
 		}
+		return result;
 	}
 }
